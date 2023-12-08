bc8b3d1de7dbd70b23caefb10570b4594530dfc9
flohki <152478430+flohki@users.noreply.github.com>
2023-12-08 16:19:52 +0100

    Update form.js

--- werkfile/form.js
+++ werkfile/form.js
@@ -1,7 +1,13 @@
 "use strict"
 
-let acc = document.getElementsByClassName("accordon");
-let i;
+let naamTxt;
+let emailTxt;
+let datumTxt;
+let uurTxt;
+let gelegenheidTxt;
+let aantalPersonenTxt;
+
+let allesCorrectIngevuld=true;
 
 function controleerVoorwaardenNaam(){
     if(naamTxt.length < 2 ){
@@ -14,23 +20,28 @@ function controleerVoorwaardenNaam(){
 }
 
 function controleerVoorwaardenEmail(){
-    if(emailTxtTxt.length < 2 ){
-        document.getElementById("email_error").innerHTML="Minstens 2 karakters lang!";
-            allesCorrectIngevuld = false;
-    }
-    else{
-        document.getElementById("email_error").innerHTML="";
-    }
-  };
-
-let voornaamTxt;
-function controleerVoorwaardenVoornaam(){
-  if(voornaamTxt.length < 2 ){
-    document.getElementById("voornaam_error").innerHTML="Minstens 2 karakters lang!";
+  let regExp=/^[A-Za-z][\.A-Za-z0-9+_-]+@[\.A-Za-z0-9-]+\.[A-Za-z]{2,20}$/;
+  if(regExp.test(emailTxt) == false){
+    document.getElementById("email_error").innerHTML="Dit is niet correct!";
     allesCorrectIngevuld = false;
   }
   else{
-    document.getElementById("voornaam_error").innerHTML="";
-  }
+    document.getElementById("email_error").innerHTML="";
+  }	
 };
+function controleerVoorwaardenDatum(){
+  let regExp=/^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/(19|20)\d{2}$/;
+  if(regExp.test(datumTxt) == false){
+    document.getElementById("datum_error").innerHTML="Dit is niet correct!";
+    allesCorrectIngevuld = false;
+  }
+  else{
+    document.getElementById("datum_error").innerHTML="";
+  }	
+};
+function controleerVoorwaardenGelegenheid(){
+  if (selectElement.value === '') {
+    document.getElementById('gelegenheid_error').innerHTML= "Kies een gelegenheid";
+  }
+}
 
